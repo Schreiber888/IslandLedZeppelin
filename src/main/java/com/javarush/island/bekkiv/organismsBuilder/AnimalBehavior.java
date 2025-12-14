9cc8bcbad02678d0acf8322a8776dcac4145ca7f
Bekk I.V. <888_1@mail.ru>
2025-12-14 21:21:29 +0500

    Add concrete class organisms

---
+++ src/main/java/com/javarush/island/bekkiv/organismsBuilder/AnimalBehavior.java
@@ -0,0 +1,13 @@
+package com.javarush.island.bekkiv.organismsBuilder;
+
+import com.javarush.island.bekkiv.animals.Organisms;
+
+import java.util.List;
+
+public interface AnimalBehavior {
+    void eat(Organisms organismsFirst, Organisms organismsSecond, List<Organisms> list);
+    void reproduce(Organisms organisms, List<Organisms> listOrganisms);
+    int[][] move(int xCell, int yCell);
+
+    Organisms build();
+}
---
+++ src/main/java/com/javarush/island/bekkiv/organismsBuilder/PlantsBehavior.java
@@ -0,0 +1,9 @@
+package com.javarush.island.bekkiv.organismsBuilder;
+
+import com.javarush.island.bekkiv.animals.Organisms;
+
+public interface PlantsBehavior {
+    int grow();
+
+    Organisms build();
+}
