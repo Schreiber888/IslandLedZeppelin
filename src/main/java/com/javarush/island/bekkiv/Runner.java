462fadeee9ededfb595a60cb9317777ebd2bc977
Bekk I.V. <888_1@mail.ru>
2026-01-19 21:43:53 +0500

    Done!

--- src/main/java/com/javarush/island/bekkiv/Runner.java
+++ src/main/java/com/javarush/island/bekkiv/Runner.java
@@ -15,6 +15,7 @@ public class Runner {
             factoryOrganisms.makeAnimals();
             System.out.println("Размер острова клеток: " + " " + FactoryOrganisms.mapAnimals.size());
             System.out.println("На моем ПК, если 2000 клеток, необходимо было подождать 50 секунд для окончания первого цикла игры и вывода количества животных на всей карте");
+            System.out.println("Когда на одной из карт не будет хищников игра закончится");
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
